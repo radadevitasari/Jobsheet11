5bd98fbc3cb1881b13c43331a5288a7361c8ea46
radadevitasari <radadevitasari87.com>
2025-11-19 19:47:01 +0700

    tambahan kode no 4

--- Numbers19.java
+++ Numbers19.java
@@ -16,6 +16,9 @@ public class Numbers19 {
     for (int i = 0; i < myNumbers.length; i++) {
        System.out.println(Arrays.toString(myNumbers[i]));
     }
+    for (int i = 0; i < myNumbers.length; i++) {
+        System.out.println("Panjang baris ke-" + (i + 1) + ": " + myNumbers[i].length);
+    }
 
     }
 }
