8fd93e626b1c709e9ad1b42ceb1eff68c0a174a4
radadevitasari <radadevitasari87.com>
2025-11-19 19:31:34 +0700

    percobaan 3

---
+++ Numbers19.java
@@ -0,0 +1,15 @@
+public class Numbers19 {
+
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][]; 
+
+        myNumbers[0] = new int[5]; 
+        myNumbers[1] = new int[3]; 
+        myNumbers[2] = new int[1]; 
+
+        System.out.println("Panjang baris 0 : " + myNumbers[0].length);
+        System.out.println("Panjang baris 1 : " + myNumbers[1].length);
+        System.out.println("Panjang baris 2 : " + myNumbers[2].length);
+    }
+}
+    
