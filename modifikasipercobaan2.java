5fa06de30930d0fc131cafc96aa4cc72902969d3
radadevitasari <radadevitasari87.com>
2025-11-19 17:16:03 +0700

    modifikasi no 3

--- modifikasipercobaan2.java
+++ modifikasipercobaan2.java
@@ -21,19 +21,20 @@ public class modifikasipercobaan2 {
                 case 1:
                     System.out.print("Masukkan nama: ");
                     String nama = sc.nextLine();
-
                     System.out.print("Masukkan baris (1-4): ");
                     int baris = sc.nextInt();
-
                     System.out.print("Masukkan kolom (1-2): ");
                     int kolom = sc.nextInt();
                     sc.nextLine();
-
+                    
+                if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
+                     System.out.println("Kursi tidak tersedia!");
+                     System.out.println("Silakan masukkan baris 1–4 dan kolom 1–2.");
+                } 
+                else {
                     penonton[baris - 1][kolom - 1] = nama;
-
                     System.out.println("Data penonton berhasil disimpan!");
-                    break;
-
+}
                 case 2:
                     System.out.println("\n=== DAFTAR PENONTON ===");
                     for (int i = 0; i < 4; i++) {
